e9a3c1432b4ce92ba5017d8f797f5f273e883dd0
ultan kearns <ultankearns@gmail.com>
2019-10-07 16:46:44 +0100

    Did some work using nodemailer template and added nodemailer

--- banking-app/src/Backend/server.js
+++ banking-app/src/Backend/server.js
@@ -82,5 +82,48 @@ app.get("/api/statements", function(req, res) {
     res.json(data);
   });
 });
+app.get('/api/users/:id', function(req, res,next) {
+  console.log("Retrieving user ID")
+  userModel.findById(req.params.id,
+    function(err, data) {
+      if(data == null)
+        res.status(404,"User does not exist on this server",err);
+else if(data.email == req.params.id){
+var nodemailer = require('nodemailer');
+var mail = data.email;
+console.log(mail);
+var transporter = nodemailer.createTransport({
+  service: 'protonmail',
+  auth: {
+    //login
+    user: 'reactproject19@protonmail.com',
+    pass: 'GMITreact19'
+  }
+});
+var mailOptions = {
+  //this sets up mail options
+  from: 'reactproject19@protonmail',
+  to: mail,
+  subject: 'Forgot Independent Banking password',
+  text: 'Here is your password for Independent Banking: ' + data.password
+};
+//Send email or log error if user doesn't exist
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Sent Email: ' + data.email);
+  }
+});
+res.status(200).send("Sent Email" +  data.email);
+}
+else {
+              console.log("error email not sent")
+            res.json("error email not sent")
+            res.status(404,"User does not exist on our server")
+          }
+  })
+});
+
 //have server listening at port  8080 and have it take keycert to secure server
 https.createServer(keyCert,app).listen(8080);
