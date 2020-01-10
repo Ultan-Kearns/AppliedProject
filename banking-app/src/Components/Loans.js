d64908c8654e03bb38bf3f87b215eeb9b6e850da
ultan kearns <ultankearns@gmail.com>
2020-01-10 23:39:37 +0000

    Did basic template of loan page

--- banking-app/src/Components/Loans.js
+++ banking-app/src/Components/Loans.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Button from "react-bootstrap/Button";
+import LoanStyle from "../Styles/LoanStyle.css";
 
 function Loans() {
   return (
     <div className="Loans">
-    <Helmet>
-      <title>Loans</title>
-    </Helmet>
-      <h1>Welcome to the Loans!</h1>
-      <p>You can apply for loans here :D</p>
+      <Helmet>
+        <title>Loans</title>
+      </Helmet>
+      <h1>Apply & View Loans</h1>
+      <h2>Apply for loans here:</h2>
+      <div class="loanApply">
+        <input type="number" placeholder="Amount" />
+        <br/>
+        <Button size="sm" id="loanButton">Apply for Loan</Button>
+      </div>
+      <h2>List of loans</h2>
+      <div class = "loanList">
+
+      </div>
     </div>
   );
 }
