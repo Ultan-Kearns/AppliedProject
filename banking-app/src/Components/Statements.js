93fceb4b07b1cf69e210aef62fd04e426291c778
ultan kearns <ultankearns@gmail.com>
2020-01-13 01:22:51 +0000

    working through statements

--- banking-app/src/Components/Statements.js
+++ banking-app/src/Components/Statements.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import { Helmet } from "react-helmet";
 
 var statements = []
-
+var test = [{location:"AAA"}]
 class Statements extends React.Component{
   constructor(props){
     super(props)
@@ -14,26 +14,30 @@ class Statements extends React.Component{
     }
   }
   componentWillMount(){
-    //connect to server and get statements upon component load
-    //use loop and check user ID to print out right statements for user
-    const axios = require('axios').default;
-    axios.get("https://localhost:8080/api/statements").then(res=>{
-      //log res for testing
-      console.log("LOCATION " + res.data[1].location)
-      console.log(res)
-        for(var i = 0; i < res.data.length; i++){
-          this.setState({
-           location:res.data[i].location,
-           cost: res.data[i].cost,
-         });
-         statements[i] =  {location:res.data[i].location,cost:res.data[i].cost}
-         console.log("I " + i + " RES " + res.data[i].cost)
-        }
-      }
-    )
+    this.getStatements()
+
+   }
+   getStatements(){
+     const axios = require('axios').default;
+     axios.get("https://localhost:8080/api/statements").then(res=>{
+       //log res for testing
+       console.log("LOCATION " + res.data[1].location)
+       console.log(res)
+         for(var i = 0; i < res.data.length; i++){
+           this.setState({
+            location:res.data[i].location,
+            cost: res.data[i].cost,
+          });
+          statements[i] =  {location:res.data[i].location,cost:res.data[i].cost}
+          console.log("TEST STATEMENTS " + statements.length + " "+  statements[i].location)
+          console.log("I " + i + " RES " + res.data[i].cost)
+         }
+       }
+     )
    }
   render() {
     return (
+      //statements populating but can't access field
       <div className="statements">
       <Helmet>
         <title>Statements</title>
@@ -42,9 +46,8 @@ class Statements extends React.Component{
         <p>
           You can view statements here : D- maybe have a dropdown to select year
           or month of statement
-          {console.log("TEST " + statements.length + " st " + statements.cost)}
-          { " Payment Amount: " + statements.cost + " Location where spent: " + this.state.location}
-        </p>
+          { " Payment Amount: "  + JSON.stringify(statements[0]) +" Location where spent: " + this.state.location}
+          </p>
       </div>
     );
   }
