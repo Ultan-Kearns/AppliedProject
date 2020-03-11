906dd37d13336e854a5747453262d8935724aca4
ultan kearns <ultankearns@gmail.com>
2020-03-11 19:08:56 +0000

    fixed up about page

--- banking-app/src/Components/About.js
+++ banking-app/src/Components/About.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import "../Styles/AboutStyle.css";
-import Image from 'react-bootstrap/Image'
-import Card from 'react-bootstrap/Card'
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 function About() {
   return (
     <div className="About">
@@ -17,79 +17,62 @@ function About() {
         differ from other banks.
       </p>
       <div className="liesWeTellCustomers" id="left">
-        <Card style={{ width: '50vw' }}>
-        <Card.Header>
-          Welcome to the only bank you'll ever need!
-        </Card.Header>
-        <Image
-          src={require("../Assets/happycustomers.jpg")}
-          alt="Pictures of happy sheep...... I mean customers!"
-        roundedCircle/>
-    <Card.Body>
-      <Card.Title>We Care About YOU!</Card.Title>
-      <Card.Text>
-        Yes we know you have heard this blaise comment before but we really
-        mean it, just because we are a massive corporation whose only
-        objective is to make a profit doesn't mean we don't care! I mean look
-        at how happy our customers are! You too can be happy if you just give
-        us all your business and a tonne of money!
-      </Card.Text>
-    </Card.Body>
-  </Card>
+        <Card style={{ width: "50vw" }}>
+          <Card.Header>Welcome to the only bank you'll ever need!</Card.Header>
+          <Image
+            src={require("../Assets/happycustomers.jpg")}
+            alt="Pictures of happy sheep...... I mean customers!"
+            roundedCircle
+          />
+          <Card.Body>
+            <Card.Title>We Care About YOU!</Card.Title>
+            <Card.Text>
+              Yes we know you have heard this blaise comment before but we
+              really mean it, just because we are a massive corporation whose
+              only objective is to make a profit doesn't mean we don't care! I
+              mean look at how happy our customers are! You too can be happy if
+              you just give us all your business and a tonne of money!
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
-      <div className="liesWeTellCustomers" id="right">
-      <Card style={{ width: '46vw' }}>
-      <Card.Header>
-        We don't just care about money, we care about OUR money!
-      </Card.Header>
-      <Image
-        src={require("../Assets/money.jpg")}
-        alt="Pictures of happy sheep...... I mean customers!"
-      roundedCircle/>
-  <Card.Body>
-    <Card.Title>We Don't Just Care About Money!</Card.Title>
-    <Card.Text>
-    Yes we know what we said before makes us seem callous! but we care
-    about your personal well-being, if something were to happen to you how
-    in the world could we make money off you? That's why we care!
-    </Card.Text>
-  </Card.Body>
-</Card>
-      </div>
-      <div className="liesWeTellCustomers" id="theBigOne">
-      <Card>
-      <Card.Header>
-        A secret just for you, and everyone else who has an account
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>Wanna hear a secret?!</Card.Title>
-        <Card.Text>
-    Pssssst............. You're getting an amazing deal!
-        </Card.Text>
-      </Card.Body>
-    </Card>
-      </div>
+        <Card style={{ width: "46vw" }} className="liesWeTellCustomers" id="right">
+          <Card.Header>
+            We don't just care about money, we care about OUR money!
+          </Card.Header>
+          <Image
+            src={require("../Assets/money.jpg")}
+            alt="Pictures of happy sheep...... I mean customers!"
+            roundedCircle
+          />
+          <Card.Body>
+            <Card.Title>We Don't Just Care About Money!</Card.Title>
+            <Card.Text>
+              Yes we know what we said before makes us seem callous! but we care
+              about your personal well-being, if something were to happen to you
+              how in the world could we make money off you? That's why we care!
+            </Card.Text>
+          </Card.Body>
+        </Card>
       <div className="liesWeTellCustomersBottom" id="left">
-      <Card>
-      <Card.Header>
-        Our evil..... I mean our plans
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>Our Aims</Card.Title>
-        <Card.Text>
-          We aim to provide you, {sessionStorage.getItem("username")}, YES YOU!
-          with a site that is:
-        </Card.Text>
-        <ul>
-          <li>
-            Responsive - A site that is accessible from any device and still
-            looks good!
-          </li>
-          <li>Secure - A site that won't leave your data compromised</li>
-          <li>Robust - A site that won't let you down</li>
-        </ul>
-      </Card.Body>
-    </Card>
+        <Card>
+          <Card.Header>Our evil..... I mean our plans</Card.Header>
+          <Card.Body>
+            <Card.Title>Our Aims</Card.Title>
+            <Card.Text>
+              We aim to provide you, {sessionStorage.getItem("username")}, YES
+              YOU! with a site that is:
+            </Card.Text>
+            <ul>
+              <li>
+                Responsive - A site that is accessible from any device and still
+                looks good!
+              </li>
+              <li>Secure - A site that won't leave your data compromised</li>
+              <li>Robust - A site that won't let you down</li>
+            </ul>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
